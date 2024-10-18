e27d878620bfb2a6bd5d774bf35574c99b9f6781
Estevao Augusto <estevaoaugusto2005@gmail.com>
2024-10-18 18:36:25 -0300

    Classes A e B adicionadas

---
+++ B.java
@@ -0,0 +1,28 @@
+class B{
+    private int B1;
+    private float B2;
+
+    public void setB2(float b2) {
+        B2 = b2;
+    }
+    
+    public float getB2() {
+        return B2;
+    }
+    
+    public void setB1(int b1) {
+        B1 = b1;
+    }
+
+    public int getB1() {
+        return B1;
+    }
+
+    public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
